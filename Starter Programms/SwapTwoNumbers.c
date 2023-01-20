ce1f4d1d1b2cd7ae3a1f68c978f22d54346358f5
pedro_.fp <a104082@alunos.uminho.pt>
2023-01-20 00:24:53 +0000

    Add SwapNumbers

---
+++ Starter Programms/SwapTwoNumbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+// C Program to swap two numbers
+
+int main() {
+
+  double first, second, temp;
+
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  temp = first;
+
+  first = second;
+
+  second = temp;
+
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+
+  return 0;
+
+}
