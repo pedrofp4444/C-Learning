16a116a4979d132449a6d6c48f56c3974117ed0d
pedro_.fp <a104082@alunos.uminho.pt>
2023-01-20 00:09:01 +0000

    Add MultiplyTwoNumbers

---
+++ MultiplyTwoNumbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// C Program to Multiply Two Numbers
+
+int main() {
+
+    double a, b, product;
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+ 
+    product = a * b;
+
+    printf("Product = %.2lf", product);
+    
+    return 0;
+}
