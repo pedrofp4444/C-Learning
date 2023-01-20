da76e710e179f5a96af37a8a69610c6cfe10bd64
pedro_.fp <a104082@alunos.uminho.pt>
2023-01-20 00:10:32 +0000

    Add MultiplyTwoNumbers

---
+++ FindAsciiValue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// C Program to Find Ascii Value
+
+int main() {  
+
+    char c;
+    
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
+}
